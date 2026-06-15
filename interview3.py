21eac42d09b285cb65440158ad09535976a2cdc4
amol410 <shindeamol410@gmail.com>
2026-06-15 11:01:32 +0530

    second commit

--- interview3.py
+++ interview3.py
@@ -5,3 +5,13 @@ for i in range(n):
 
 for i in range(1, n+1):
     print(i, end="")    
+
+
+# n = 5 
+
+# for i in range(n):
+#     print(i, end="")
+
+# for i in range(1, n+1):
+#     print(i, end="")    
+
